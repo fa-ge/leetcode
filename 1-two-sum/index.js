6f03027b0eba0ec20f1a63906a69e1e3460b37e8
fage <924053302@qq.com>
2016-10-10 23:51:41 +0800

    two sum题目解决

---
+++ 1-two-sum/index.js
@@ -0,0 +1,33 @@
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ * 双指针扫描
+ */
+
+var twoSum = function(nums, target) {
+    const numsBak = nums.slice(0)
+    numsBak.sort((a, b) => a - b)
+
+    const len = numsBak.length
+    let left = 0
+    let right = len - 1
+    let tar = numsBak[left] + numsBak[right]
+
+    while (tar !== target) {
+        tar > target ? right-- : left++
+        if (left >= right) {
+            break
+        }
+        tar = numsBak[left] + numsBak[right]
+    }
+
+    const index1 = nums.indexOf(numsBak[left])
+    const index2 = nums.lastIndexOf(numsBak[right])
+
+    return [Math.min(index1, index2), Math.max(index1, index2)]
+};
+
+var nums = [0,4,3,0], target = 0
+console.log(twoSum(nums, target))
