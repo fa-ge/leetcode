0163e903bbcd28284bd09a20e8c792e53b747189
fage <924053302@qq.com>
2016-10-17 22:50:55 +0800

    ac Search Insert Position

---
+++ 35-Search-Insert-Position/index.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+function bSearchRange(arr, target) {
+    let low = 0
+    let high = arr.length - 1
+    let mid
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2)
+        if (target > arr[mid]) {
+            low = mid + 1
+        } else if (target < arr[mid]) {
+            high = mid - 1
+        } else {
+            return mid
+        }
+    }
+    return low
+}
+var searchInsert = function(nums, target) {
+    return bSearchRange(nums, target)
+};
+
+console.log(searchInsert([1,3,5,6], 2))
