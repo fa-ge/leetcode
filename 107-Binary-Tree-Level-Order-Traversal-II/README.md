f65c7be531782e5d837f3c1c9fca0b8acaf8fa68
fage <924053302@qq.com>
2016-11-02 14:02:46 +0800

    ac Binary Tree Level Order Traversal II

---
+++ 107-Binary-Tree-Level-Order-Traversal-II/README.md
@@ -0,0 +1,23 @@
+### 107\. Binary Tree Level Order Traversal II
+
+Given a binary tree, return the *bottom-up level order* traversal of its nodes' values. (ie, from left to right, level by level from leaf to root).
+
+For example:
+Given binary tree `[3,9,20,null,null,15,7]`,
+
+        3
+       / \
+      9  20
+        /  \
+       15   7
+
+return its bottom-up level order traversal as:
+
+    [
+      [15,7],
+      [9,20],
+      [3]
+    ]
+
+### 题意
+和第一题基本一样。把结果的二维数组反转一下就好了
---
+++ 107-Binary-Tree-Level-Order-Traversal-II/index.js
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+    if (!root) {
+        return []
+    }
+    const res = []
+    let queue = [root]
+    let tmpQueue = []
+
+    let n = 0
+    while (queue.length !== 0) {
+        tmpQueue = []
+        res[n] = []
+        while (root = queue.shift()) {
+            res[n].push(root.val)
+            root.left && tmpQueue.push(root.left)
+            root.right && tmpQueue.push(root.right)
+        }
+        queue = tmpQueue
+        n++
+    }
+    return res.reverse()
+};
+
+function TreeNode(val) {
+    this.val = val;
+    this.left = this.right = null;
+}
+const root = new TreeNode(1)
+root.left = new TreeNode(2)
+const node1 = root.right = new TreeNode(4)
+node1.left = new TreeNode(3)
+
+console.log(levelOrderBottom(root))
