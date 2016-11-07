f0b94698245bc915b5c33d912cb680d66265720d
fage <924053302@qq.com>
2016-11-07 18:31:32 +0800

    ac Intersection of Two Arrays

---
+++ 349-Intersection-of-Two-Arrays/README.md
@@ -0,0 +1,18 @@
+### 349\. Intersection of Two Arrays
+
+
+Given two arrays, write a function to compute their intersection.
+
+Example:
+Given *nums1* = `[1, 2, 2, 1]`, *nums2* = `[2, 2]`, return `[2]`.
+
+Note:
+
+* Each element in the result must be unique.
+* The result can be in any order.
+
+### 题意
+求两个数组的交集
+
+### 方法（一）
+特别简单，把一个数组放到map中，另一个数组中的元素一个个从map中找。
---
+++ 349-Intersection-of-Two-Arrays/index.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    const map = {}
+    const set = new Set()
+
+    nums1.forEach((value, index) => map[value] = true)
+    nums2.forEach((value, index) => {
+        if (map[value]) {
+            set.add(value)
+        }
+    })
+
+    return Array.from(set)
+};
+
+console.log(intersection([1,2,3], [2,3,4]))
